390bf6a912d4de9efcdaa1c3cc6c30b07c7dcd7c
set <set>
2021-02-03 17:06:27 +0800

    update

--- src/views/Github.vue
+++ src/views/Github.vue
@@ -4,8 +4,8 @@
             <el-main>
                 <h2>App Github链接: </h2>
                 <el-link style="font-size: large" type="primary" href="https://github.com/hxy112292/wallet-ionic">https://github.com/hxy112292/wallet-ionic</el-link>
-                <h2>后端 Github链接: </h2>
-                <el-link style="font-size: large" type="primary" href="https://github.com/hxy112292/wallet-springboot">https://github.com/hxy112292/wallet-springboot</el-link>
+                <h2>部分后端 Github链接: </h2>
+                <el-link style="font-size: large" type="primary" href="https://github.com/hxy112292/wallet-backend">https://github.com/hxy112292/wallet-backend</el-link>
             </el-main>
         </el-container>
     </div>
